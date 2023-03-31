45ec4d97e491db168bb67bbbc2b7fc73114f709a
nimalchrist <chiristonimal@gmail.com>
2023-03-31 21:54:48 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,29 @@
+var config = {
+  texts: [
+    "Hey, Beautiful",
+    "Wish you a Great year Ahead",
+    "Vayasagite iruku urunda",
+    "But 22 tha aaguthu...",
+    "Enaya vida nee kutti pilla thaa",
+    "Ethra years aanalu wait panuve...with the same something....",
+    "Your eyes, nose, lips, face, hair and my chellakutti and everything...:))",
+    "seri poru poru rmba overaa pothu....",
+    "With too much of something",
+    "Your bestie",
+    "Nimal :)",
+  ],
+
+  imgs: {
+    Beautiful: "./imgs/xiaokeai.png",
+  },
+  desc: {
+    turn_on: "Inga click panunga",
+    play: "Play panu urunda",
+    bannar_coming: "Banner pakanuma busku",
+    balloons_flying: "Baloon parakratha parunga...",
+    cake_fadein: "Oruka kooda click panu",
+    light_candle: "Seri enoda wish ku wait karo",
+    wish_message: "Many more happy returns of the day Zzze u",
+    story: "Vanga kuti story pakalam....",
+  },
+};
